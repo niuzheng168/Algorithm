65252c475412f9d49d52e494f05f1718a39b6285
niuzheng168 <niuzheng168@gmail.com>
2014-08-19 18:06:01 +0800

    Add sort classes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,108 +7,10 @@
 namespace HelloWorld
 {
     using System;
-    using System.Collections.Generic;
 
     /// <summary>
     /// </summary>
-    public class PronInfo : IComparable<PronInfo>
-    {
-        #region Constructors and Destructors
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PronInfo"/> class.
-        /// </summary>
-        /// <param name="pron">
-        /// The pron.
-        /// </param>
-        /// <param name="word">
-        /// The word.
-        /// </param>
-        /// <param name="example">
-        /// The example.
-        /// </param>
-        public PronInfo(string pron, string word, string example)
-        {
-            this.Pronunciation = pron;
-            this.Count = 0;
-            this.Persentage = 0;
-            this.IsMatch = false;
-            this.IsPreferred = false;
-            this.Example = example;
-            this.Word = word;
-        }
-
-        #endregion
-
-        #region Public Properties
-
-        /// <summary>
-        /// Gets or sets the count.
-        /// </summary>
-        public int Count { get; set; }
-
-        /// <summary>
-        /// Gets or sets the example.
-        /// </summary>
-        public string Example { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether is match.
-        /// </summary>
-        public bool IsMatch { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether is preferred.
-        /// </summary>
-        public bool IsPreferred { get; set; }
-
-        /// <summary>
-        /// Gets or sets the persentage.
-        /// </summary>
-        public double Persentage { get; set; }
-
-        /// <summary>
-        /// Gets or sets the pronunciation.
-        /// </summary>
-        public string Pronunciation { get; set; }
-
-        /// <summary>
-        /// Gets or sets the word.
-        /// </summary>
-        public string Word { get; set; }
-
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// </summary>
-        /// <param name="send">
-        /// The send.
-        /// </param>
-        /// <returns>
-        /// </returns>
-        public int CompareTo(PronInfo send)
-        {
-            if (send.Count == this.Count)
-            {
-                return 0;
-            }
-
-            if (send.Count > this.Count)
-            {
-                return 1;
-            }
-
-            return -1;
-        }
-
-        #endregion
-    }
-
-    /// <summary>
-    /// </summary>
-    internal class Program
+    public class Program
     {
         #region Methods
 
@@ -117,24 +19,111 @@ namespace HelloWorld
         /// <param name="args">
         /// The args.
         /// </param>
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var list = new List<PronInfo>();
-            var pron = new PronInfo("a", "b", "c");
-            pron.Count = 1;
-            list.Add(pron);
-            pron = new PronInfo("a", "b", "c");
-            pron.Count = 22;
-            list.Add(pron);
-            pron = new PronInfo("a", "b", "c");
-            pron.Count = 3;
-            list.Add(pron);
-            list.Sort();
-            foreach (PronInfo pr in list)
+            // Create an unsorted array of string elements
+            int[] unsorted = { 1, 2, 6, 3, 4, 5, 9, 12, 13, 14, 15 };
+
+            // Sort the array
+            Quicksort2(unsorted, 0, unsorted.Length - 1);
+
+            // Print the sorted array
+            for (int i = 0; i < unsorted.Length; i++)
             {
-                Console.WriteLine(pr.Count);
+                Console.Write(unsorted[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        public static int partition(int[] arr, int start, int end)
+        {
+            // We assume last element of array as pivot element
+            int pivot = (start + end) / 2;
+            int i = start, j = end, temp;
+            while (i < j)
+            {
+                // traverse the array and find index where element is greater than pivot element
+                while (i < end && arr[i] < arr[pivot])
+                    i++;
+                // traverse the array and find index where element is smaller than pivot element
+                while (j > start && arr[j] > arr[pivot])
+                    j--;
+
+                //exchange elements on indexes found by i and j
+                if (i < j)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            // finally put pivot element at its right position in array
+            temp = arr[pivot];
+            arr[pivot] = arr[j];
+            arr[j] = temp;
+            // return pivot index
+            return j;
+        }
+        // Quick sort procedure
+        public static void Quicksort2(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                // find the pivot index
+                int pivotIndex = partition(arr, start, end);
+                // recursivly call itself for array element before pivot index
+                Quicksort2(arr, start, pivotIndex - 1);
+                // recursivly call itself for array element after pivot index
+                Quicksort2(arr, pivotIndex + 1, end);
             }
         }
+
+
+        public static void Quicksort(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(elements, i, right);
+            }
+        }
+
+
 
         #endregion
     }
