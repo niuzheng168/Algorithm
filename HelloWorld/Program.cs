7b4f967842ebcb26fbcb1e2d1959c375aaffa445
niuzheng168 <niuzheng168@gmail.com>
2014-08-22 16:36:55 +0800

    Add quick sort and merge sort

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,8 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Program.cs" company="">
-//   
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
-    using System;
+    using System.Collections.Generic;
+
+    using HelloWorld.Sort;
 
     /// <summary>
     /// </summary>
@@ -19,111 +15,17 @@ namespace HelloWorld
         /// <param name="args">
         /// The args.
         /// </param>
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // Create an unsorted array of string elements
-            int[] unsorted = { 1, 2, 6, 3, 4, 5, 9, 12, 13, 14, 15 };
+            int[] unsorted = { 10, 2, 8, 6, 4, 7, 9, 3, 5, 7 };
 
-            // Sort the array
-            Quicksort2(unsorted, 0, unsorted.Length - 1);
+            // int[] unsorted = { 10, 2 };
+            List<int> list = new List<int>(unsorted);
 
-            // Print the sorted array
-            for (int i = 0; i < unsorted.Length; i++)
-            {
-                Console.Write(unsorted[i] + " ");
-            }
-
-            Console.WriteLine();
-
-            Console.ReadLine();
+            list.MergeSort();
+            list.QuickSort(0, list.Count - 1);
         }
-
-        public static int partition(int[] arr, int start, int end)
-        {
-            // We assume last element of array as pivot element
-            int pivot = (start + end) / 2;
-            int i = start, j = end, temp;
-            while (i < j)
-            {
-                // traverse the array and find index where element is greater than pivot element
-                while (i < end && arr[i] < arr[pivot])
-                    i++;
-                // traverse the array and find index where element is smaller than pivot element
-                while (j > start && arr[j] > arr[pivot])
-                    j--;
-
-                //exchange elements on indexes found by i and j
-                if (i < j)
-                {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-            // finally put pivot element at its right position in array
-            temp = arr[pivot];
-            arr[pivot] = arr[j];
-            arr[j] = temp;
-            // return pivot index
-            return j;
-        }
-        // Quick sort procedure
-        public static void Quicksort2(int[] arr, int start, int end)
-        {
-            if (start < end)
-            {
-                // find the pivot index
-                int pivotIndex = partition(arr, start, end);
-                // recursivly call itself for array element before pivot index
-                Quicksort2(arr, start, pivotIndex - 1);
-                // recursivly call itself for array element after pivot index
-                Quicksort2(arr, pivotIndex + 1, end);
-            }
-        }
-
-
-        public static void Quicksort(int[] elements, int left, int right)
-        {
-            int i = left, j = right;
-            int pivot = elements[(left + right) / 2];
-
-            while (i <= j)
-            {
-                while (elements[i].CompareTo(pivot) < 0)
-                {
-                    i++;
-                }
-
-                while (elements[j].CompareTo(pivot) > 0)
-                {
-                    j--;
-                }
-
-                if (i <= j)
-                {
-                    // Swap
-                    int tmp = elements[i];
-                    elements[i] = elements[j];
-                    elements[j] = tmp;
-
-                    i++;
-                    j--;
-                }
-            }
-
-            // Recursive calls
-            if (left < j)
-            {
-                Quicksort(elements, left, j);
-            }
-
-            if (i < right)
-            {
-                Quicksort(elements, i, right);
-            }
-        }
-
-
 
         #endregion
     }
---
+++ HelloWorld/Sort/SortMethods.cs
@@ -0,0 +1,224 @@
+﻿namespace HelloWorld.Sort
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     The quick sort.
+    /// </summary>
+    public static class SortMethods
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The quick sort.
+        /// </summary>
+        /// <param name="list">
+        /// The list.
+        /// </param>
+        /// <param name="left">
+        /// The left.
+        /// </param>
+        /// <param name="right">
+        /// The right.
+        /// </param>
+        /// <param name="cmp">
+        /// The cmp.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        public static void QuickSort<T>(this IList<T> list, int left, int right, IComparer<T> cmp)
+        {
+            int i = left;
+            int j = right;
+
+            int pivotIndex = (left + right) / 2;
+            T pivot = list[pivotIndex];
+
+            while (i < j)
+            {
+                while (cmp.Compare(list[i], pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (cmp.Compare(list[j], pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i < j)
+                {
+                    T tmp = list[i];
+                    list[i] = list[j];
+                    list[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+            {
+                QuickSort(list, left, j, cmp);
+            }
+
+            if (j + 1 < right)
+            {
+                QuickSort(list, j + 1, right, cmp);
+            }
+        }
+
+        /// <summary>
+        /// The quick sort.
+        /// </summary>
+        /// <param name="list">
+        /// The list.
+        /// </param>
+        /// <param name="left">
+        /// The left.
+        /// </param>
+        /// <param name="right">
+        /// The right.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        public static void QuickSort<T>(this IList<T> list, int left, int right) where T : IComparable<T>
+        {
+            int i = left;
+            int j = right;
+
+            int pivotIndex = (left + right) / 2;
+            T pivot = list[pivotIndex];
+
+            while (i < j)
+            {
+                while (list[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (list[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i < j)
+                {
+                    T tmp = list[i];
+                    list[i] = list[j];
+                    list[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+            {
+                QuickSort(list, left, j);
+            }
+
+            if (j + 1 < right)
+            {
+                QuickSort(list, j + 1, right);
+            }
+        }
+
+        /// <summary>
+        /// The merge sort.
+        /// </summary>
+        /// <param name="list">
+        /// The list.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        public static void MergeSort<T>(this IList<T> list) where T : IComparable<T>
+        {
+            if (list.Count <= 1)
+            {
+                return;
+            }
+
+            int mid = list.Count / 2;
+            IList<T> LList = new List<T>();
+            IList<T> RList = new List<T>();
+
+            for (int i = 0; i < mid; i++)
+            {
+                LList.Add(list[i]);
+            }
+
+            for (int i = mid; i < list.Count; i++)
+            {
+                RList.Add(list[i]);
+            }
+
+            LList.MergeSort();
+            RList.MergeSort();
+
+            list.Clear();
+            foreach (var item in MergeSort_MergeList(LList, RList))
+            {
+                list.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// The merge sort_ merge list.
+        /// </summary>
+        /// <param name="a">
+        /// The a.
+        /// </param>
+        /// <param name="b">
+        /// The b.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="IList"/>.
+        /// </returns>
+        public static IList<T> MergeSort_MergeList<T>(IList<T> a, IList<T> b) where T : IComparable<T>
+        {
+            IList<T> list = new List<T>();
+
+            int i, j;
+            for (i = 0, j = 0; i < a.Count && j < b.Count;)
+            {
+                T item1 = a[i];
+                T item2 = b[j];
+
+                if (item1.CompareTo(item2) <= 0)
+                {
+                    list.Add(item1);
+                    i++;
+                }
+                else
+                {
+                    list.Add(item2);
+                    j++;
+                }
+            }
+
+            if (i < a.Count)
+            {
+                for (; i < a.Count; i++)
+                {
+                    list.Add(a[i]);
+                }
+            }
+
+            if (j < b.Count)
+            {
+                for (; j < b.Count; j++)
+                {
+                    list.Add(b[j]);
+                }
+            }
+
+            return list;
+        }
+
+        #endregion
+    }
+}
